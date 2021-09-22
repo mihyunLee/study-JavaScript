52ec3b231978a736eef24edb876f936a84db8b63
mihyun <mihy621@gmail.com>
2021-09-22 21:48:57 +0900

    Variables, DataType on JS

---
+++ index.js
@@ -0,0 +1,27 @@
+// #1-7. variables
+const a = 250;
+let b = a - 5;
+//a = 4;
+console.log(b, a);
+
+
+// #1-8. Data Type
+//String
+const what = "Mihyun";
+const emoji = "😉";
+
+console.log(what);
+console.log(emoji);
+
+//Boolean
+const wat1 = true; //true: 1, false: 0
+console.log(wat1);
+
+//Number
+const wat2 = 666;
+console.log(wat2);
+
+//Float
+const wat3 = 3.14;
+console.log(wat3);
+
