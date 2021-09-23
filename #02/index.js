c4c8ffe0f214a47501a82fbd47ac5e5be2334148
mihyun <mihy621@gmail.com>
2021-09-23 23:19:25 +0900

    DOM with JS

--- #02/index.js
+++ #02/index.js
@@ -1,40 +1,14 @@
-// #2.1 function
-// function sayHello(name, age){
-//     console.log("Hello!", name, "you have", age, "years of age.");
-//     console.log(`Hello ${name} you are ${age} years old`);
-// }
+// # 2.2 Dom Function
+// const title = document.getElementById('title');
+// title.innerHTML = "Hi! From JS!";
+// console.log(title);
 
-// sayHello("Mihyun", 20);
+// # 2.3 Modifying the DOM with JS
+//const title = document.getElementById('title');
+const title = document.querySelector('#title');
+title.innerHTML = "Hi! From JS!";
+title.style.color = 'red';
+document.title = 'I won you now'
 
-function sayHello(name, age){
-    return `Hello ${name} you are ${age} years old`;
-}
-
-const greetMan = sayHello("Mihyun", 20);
-console.log(greetMan);
-
-const calculrator = {
-    plus: function(a, b){
-        return a + b;
-    },
-    minus: function(a, b){
-        return a - b;
-    },
-    multi: function(a, b){
-        return a * b;
-    },
-    division: function(a, b){
-        return a / b;
-    },
-    expo: function(a, b){
-        return a**b;
-    }
-};
-
-const result1 = calculrator.plus(5, 5);
-const result2 = calculrator.minus(7, 5);
-const result3 = calculrator.multi(6, 3);
-const result4 = calculrator.division(18, 2);
-const result5 = calculrator.expo(2, 3);
-console.log(result1, result2, result3, result4, result5);
+console.dir(document);
 
