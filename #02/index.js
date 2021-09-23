26c0acbeb7729fb90be669aee20b7e1eb28f162f
mihyun <mihy621@gmail.com>
2021-09-24 00:18:16 +0900

    Events and event handlers

--- #02/index.js
+++ #02/index.js
@@ -5,10 +5,26 @@
 
 // # 2.3 Modifying the DOM with JS
 //const title = document.getElementById('title');
+// const title = document.querySelector('#title');
+// title.innerHTML = "Hi! From JS!";
+// title.style.color = 'red';
+// document.title = 'I won you now'
+
+// console.dir(document);
+
+//# 2.4 Events and event handlers
+
+// function handleResize(event){
+//     //console.log("I have been resiezed");
+//     console.log(event);
+// }
+
+//window.addEventListener("resize", handleResize);
+
 const title = document.querySelector('#title');
-title.innerHTML = "Hi! From JS!";
-title.style.color = 'red';
-document.title = 'I won you now'
 
-console.dir(document);
+function handleClick(){
+    title.style.color = 'blue';
+}
 
+title.addEventListener("click", handleClick);
